a40ad958a7296955a9feeced27dde8151f97a5ee
Martín Stancanelli <mastanca14@gmail.com>
2020-01-26 16:32:01 -0300

    Add move forward block

--- src/client/components/Blockly/initContent/content.jsx
+++ src/client/components/Blockly/initContent/content.jsx
@@ -12,6 +12,7 @@ Blockly.Blocks.rotate_arpb2 = {
       .appendField('rotate')
       .appendField(new Blockly.FieldDropdown([['left', 'rotate_left'], ['right', 'rotate_right']]), 'direction');
     this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(0);
     this.setTooltip('Rotate ARPB2 in the selected direction');
     this.setHelpUrl('');
@@ -20,8 +21,23 @@ Blockly.Blocks.rotate_arpb2 = {
 
 Blockly.JavaScript.rotate_arpb2 = (block) => {
   const dropdown_direction = block.getFieldValue('direction');
-  // TODO: Assemble JavaScript into code variable.
-  const code = '';
+  const code = `(direction) => { ACTION_ROTATE_${dropdown_direction.toUpperCase()}; }`;
+  return code;
+};
+
+Blockly.Blocks.move_forward_arpb2 = {
+  init() {
+    this.appendDummyInput().appendField('move forward');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+    this.setTooltip('Move ARPB2 in the current direction');
+    this.setHelpUrl('');
+  },
+};
+
+Blockly.JavaScript.move_forward_arpb2 = (block) => {
+  const code = '() => { ACTION_MOVE_FORWARD; }';
   return code;
 };
 
