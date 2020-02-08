7d345d96697d1a939914da29a494ebb414a3fdc0
Martín Stancanelli <mastanca14@gmail.com>
2020-02-08 19:52:14 -0300

    Fix path

--- src/server/code/index.js
+++ src/server/code/index.js
@@ -3,7 +3,7 @@ import CodeService from '../services/code';
 
 const router = express.Router();
 
-router.post('/save', (req, res, next) => {
+router.post('/', (req, res, next) => {
   CodeService.save(req.body.code, req.body.workspace, req.body.userId)
     .then(payload => res.json(payload))
     .catch(err => res.status(err.statusCode ? err.statusCode : 500).json({ error: err.message }));
