ea80776ca3bfb85d518558c5ae5672e5c51a6cb3
Martín Stancanelli <mastanca14@gmail.com>
2020-03-13 21:12:07 -0300

    Remove old export

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,4 +1,3 @@
 export { default as SearchInput } from './SearchInput';
 export { default as StatusBullet } from './StatusBullet';
 export { default as RouteWithLayout } from './RouteWithLayout';
-export { default as MyBlockly } from './Blockly';
