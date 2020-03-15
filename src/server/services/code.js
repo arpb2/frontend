709e9e9c06cc1fb9701f803c7a56f09e39bc5b69
Martín Stancanelli <mastanca14@gmail.com>
2020-03-15 18:44:45 -0300

    Connect to backend on level get and code save

--- src/server/services/code.js
+++ src/server/services/code.js
@@ -2,28 +2,23 @@ import fetch from 'node-fetch';
 
 export default class CodeService {
   static save(code, workspace, userId, levelId, token) {
+    const body = JSON.stringify({
+      code,
+      workspace,
+      level_id: levelId,
+      user_id: userId,
+    });
     return new Promise((resolve, reject) => {
       fetch(`${process.env.BACKEND_HOST}/users/${userId}/levels/${levelId}`, {
         method: 'PUT',
-        body: JSON.stringify({
-          code,
-          workspace,
-        }),
+        body,
         headers: {
-          Authentication: token,
+          Authorization: 'DEBUG', // token,
           'Content-Type': 'application/json',
         },
       })
-        // .then((res) => {
-        //   // TODO: Delete this then when back is up and uncomment the following lines
-        //   resolve({
-        //     code,
-        //     code_id: 1987,
-        //     userId,
-        //   });
-        // })
         .then((res) => {
-          if (!res.ok) reject(res.statusText);
+          if (!res.ok) reject(res);
           return res.json();
         })
         .then((data) => {
