db7d75495233166dc357fcc0ea50be75e867ca53
Martín Stancanelli <mastanca14@gmail.com>
2020-03-24 20:11:39 -0300

    Fix deprecated use

--- src/server/classrooms/index.js
+++ src/server/classrooms/index.js
@@ -4,7 +4,7 @@ import ClassroomService from '../services/classrooms';
 const router = express.Router();
 
 router.get('/:id', (req, res, next) => {
-  ClassroomService.getById(req.param('id'), req.header('Authorization'))
+  ClassroomService.getById(req.params.id, req.header('Authorization'))
     .then(payload => res.json(payload))
     .catch(err => next(err));
 });
