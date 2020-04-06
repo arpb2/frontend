0fd6565b5b3e20be59b1f9ee287953d8371997c0
Martín Stancanelli <mastanca14@gmail.com>
2020-04-06 18:53:39 -0300

    Remove hardcoded teacher role

--- src/client/common/auth.js
+++ src/client/common/auth.js
@@ -1,7 +1,8 @@
-export const isTeacher = () =>
-  // const loggedInUser = JSON.parse(localStorage.getItem('session'));
-  // return loggedInUser.type === 'teacher';
-  true;
+export const isTeacher = () => {
+  const loggedInUser = JSON.parse(localStorage.getItem('session'));
+  return loggedInUser.type === 'teacher';
+};
+
 export const isStudent = () => {
   const loggedInUser = JSON.parse(localStorage.getItem('session'));
   return loggedInUser.type === 'student';
