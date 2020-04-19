3f0f6aa767d0ed83bf281598f6fbf9090dab77cb
Martín Stancanelli <mastanca14@gmail.com>
2020-04-19 18:59:09 -0300

    Move mesasging script to own js file

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -11,7 +11,6 @@ import './assets/scss/index.scss';
 import validators from './common/validators';
 import Routes from './Routes';
 
-
 Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
   draw: chartjs.draw,
 });
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
+import messaging from './messaging';
 import App from './App';
+
+messaging.requestPermission();
 
 serviceWorker.register();
 
